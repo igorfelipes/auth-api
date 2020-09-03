1edf20d49fcb5aba71ef390b9a6ffb5f0cfc7770
Igor Sales <igorsales.fs@gmail.com>
2020-09-03 17:26:11 -0300

    add env variables

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,7 @@
 import mongoose, { mongo } from 'mongoose';
 
-const uri =   'mongodb+srv://admin:admin@clusterbase-hj6e6.mongodb.net/authdb?retryWrites=true&w=majority'
 
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
 
 mongoose.connection.on(
     'error', console.error.bind(console, 'MongoDB connection error: ')
